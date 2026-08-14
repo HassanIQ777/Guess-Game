6622c3450a72304bd2f3140a2019841298394f57
HassanIQ777 <hassaniq793@gmail.com>
2026-08-14 23:02:26 +0300

    Few changes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <ctime>
+#include <iostream>
 
 using namespace std;
 
@@ -7,54 +7,54 @@ int totalTries = 0;
 int gamesPlayed = 0;
 
 void game() {
-    int n, guess;
-    int tries = 0;
-    srand(time(NULL));
-    cout << "Range: ";
-    cin >> n;
-    if (n > 0) {
-        int randNum = rand() % n + 1;
-        cout << "*****************Guessing Game****************\n";
-        do {
-            cout << "Guess: ";
-            cin >> guess;
-            if (guess == randNum) {
-                cout << "\nCongrats you won!\n";
-            } else if (guess > randNum) {
-                cout << "\nToo Large!\n";
-            } else if (guess < randNum) {
-                cout << "\nToo Small!\n";
-            }
-            tries += 1;
-            totalTries += 1;
-        } while (guess != randNum);
-        gamesPlayed += 1;
-        if (tries == 1) {
-            cout << "\nAmazing! You won in " << tries << " try.\n";
-        } else {
-            cout << "You won in " << tries << " tries.\n";
-        }
-        cout << "**********************************************\n";
+  int n, guess;
+  int tries = 0;
+  srand(static_cast<unsigned int>(time(NULL)));
+  cout << "Range: ";
+  cin >> n;
+  if (n > 0) {
+    int randNum = rand() % n + 1;
+    cout << "***************** Guessing Game ****************\n";
+    do {
+      cout << "Guess: ";
+      cin >> guess;
+      if (guess == randNum) {
+        cout << "\nCongrats you won!";
+      } else if (guess > randNum) {
+        cout << guess << " is too Large!\n";
+      } else if (guess < randNum) {
+        cout << guess << " is too Small!\n";
+      }
+      cout << "\n";
+      tries += 1;
+      totalTries += 1;
+    } while (guess != randNum);
+    gamesPlayed += 1;
+    if (tries == 1) {
+      cout << "\nAmazing! You won in " << tries << " try.\n";
     } else {
-        cout << "Error: Invalid range, please try again.\n";
+      cout << "You won in " << tries << " tries.\n";
     }
+    cout << "**********************************************\n";
+  } else {
+    cout << "Error: Invalid range, please try again.\n";
+  }
 }
 
 int main() {
-    string repeat="y";
-    do {
-        game();
-        cout << "Play again?[y/n]\n> ";
-        cin >> repeat;
-    } while(repeat == "y" || repeat == "Y");
-    if (gamesPlayed > 0) {
-        double averageTries = static_cast<double>(totalTries)/gamesPlayed;
-        cout << "\nStats:\nTotal games played: " << gamesPlayed << endl;
-        cout << "Total number of tries: " << totalTries << endl;
-        cout << "Average tries per game: " << averageTries << endl;
-    } else {
-        cout << "No games played.\n";
-    }
-    cout<< "\nThanks for playing!";
-    return 0;
+  string repeat = "y";
+  do {
+    game();
+    cout << "Play again? [y/n]\n> ";
+    cin >> repeat;
+  } while (repeat == "y" || repeat == "Y");
+  if (gamesPlayed > 0) {
+    double averageTries = static_cast<double>(totalTries) / gamesPlayed;
+    cout << "\n[Stats]: Total games played: " << gamesPlayed << endl;
+    cout << "[Stats]: Total number of tries: " << totalTries << endl;
+    cout << "[Stats]: Average tries per game: " << averageTries << endl;
+  } else {
+    cout << "[Stats]: No games played.\n";
+  }
+  cout << "\nThanks for playing!\n";
 }
